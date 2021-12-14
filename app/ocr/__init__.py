30d3de55a5f9c2b175156c6a16dcbd598550de43
Bright@huawei <brt2@qq.com>
2021-12-14 16:38:09 +0800

    bugfix: 修复paddleocr的依赖requirements；取消Sniper多继承关系

--- app/ocr/__init__.py
+++ app/ocr/__init__.py
@@ -13,43 +13,42 @@ def pixmap2ndarray(pixmap):
     img = Image.fromqpixmap(pixmap)
     return np.asarray(img)
 
-class PaddleOcrSnipper(ISnipper, ScreenShotWidget, TextSystem):
-    def __init__(self, *args_, **kwargs):
-        super(ISnipper, self).__init__(*args_, **kwargs)
+dir_curr = os.path.dirname(__file__)
 
+def rpath(rpath):
+    return os.path.join(dir_curr, rpath)
+
+class PaddleOcrSnipper(ISnipper, ScreenShotWidget):
+    def __init__(self, *args_, **kwargs):
+        super().__init__(*args_, **kwargs)
+        self.init_ocr_engine()
+
+    def init_ocr_engine(self):
         args = parse_args()
         args.use_gpu = False
-
-        dir_curr = os.path.dirname(__file__)
-        args.cls_model_dir = os.path.join(dir_curr, "models/ch_PP-OCRv2/ch_ppocr_mobile_v2.0_cls_infer")
-        args.det_model_dir = os.path.join(dir_curr, "models/ch_PP-OCRv2/ch_PP-OCRv2_det_infer")
-        args.rec_model_dir = os.path.join(dir_curr, "models/ch_PP-OCRv2/ch_PP-OCRv2_rec_infer")
-        super(TextSystem, self).__init__(args)
-
-    # def file2text(self, path_img):
-    #     im = cv2.imread(path_img)
-    #     return self.img2text(im)
-
-    def img2text(self, im):
-        text_lines = []
-        result = self.__call__(im)  # det & rec
-        if result is not None:
-            dt_box, tuple_rec = result
-            for text, score in tuple_rec:
-                print(f"[{score:.2f}] {text}")
-                text_lines.append(text)
-        return "\n".join(text_lines)
+        args.cls_model_dir = rpath("models/ch_PP-OCRv2/ch_ppocr_mobile_v2.0_cls_infer")
+        args.det_model_dir = rpath("models/ch_PP-OCRv2/ch_PP-OCRv2_det_infer")
+        args.rec_model_dir = rpath("models/ch_PP-OCRv2/ch_PP-OCRv2_rec_infer")
+        args.rec_char_dict_path = rpath("ppocr/utils/ppocr_keys_v1.txt")
+        self.ocr_engine = TextSystem(args)
 
     def run(self):
         self.take_snapshot()
 
     def processing(self, im_pixmap):
         im = pixmap2ndarray(im_pixmap)
-        text_lines = self.img2text(im)
+
+        list_lines = []
+        result = self.ocr_engine(im)  # det & rec
+        if result is not None:
+            dt_box, tuple_rec = result
+            for text, score in tuple_rec:
+                print(f"[{score:.2f}] {text}")
+                list_lines.append(text)
+        text_lines = "\n".join(list_lines)
 
         pyperclip.copy(text_lines)
         self.get_tray().make_msg(text_lines)
-
 
 def make_snipper(win):
     return PaddleOcrSnipper(win)
